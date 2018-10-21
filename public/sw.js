f7dc2fec5770df08a01dbe89fc76a31fc5c9dc3d
tobiaskusnaman <tobiaskusnaman@gmail.com>
2018-10-21 08:29:39 +0700

    pwa

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,16 @@
-const CACHE_NAME = 'my-site-cache-v1'
-// const filesToCache = [
-//   '../index.html',
-//   '../main.css',
-//   '../images',
-//   '../project1',
-//   './index.html',
-//   './main.css',
-//   './main.js'
-// ]
-
+const CACHE_NAME = 'my-site-cache-v2'
 const filesToCache = [
-  './images',
-  './project1',
-  './project2',
-  'index.html',
-  'main.css',
-  'main.js'
+  './images/icon.png',
+  './images/menu.svg',
+  './project1/index.html',
+  './project1/add2numbers.js',
+  './project2/index.html',
+  './project2/main.css',
+  './project2/main.js',
+  './index.html',
+  './main.css',
+  './main.js',
+  './manifest.json',
 ]
 
 self.addEventListener('install', event => {
@@ -28,58 +23,23 @@ self.addEventListener('install', event => {
   );
 })
 
-// self.addEventListener('fetch', event => {
-//   console.log('TETAEWTAWETAES ==============')
-//   console.log('Fetching:21312321321', event.request.url)
-  // event.respondWith(
-  //   caches.match(event.request)
-  //     .then(response => {
-  //       console.log('isi response', response)
-  //       if (response) {
-  //         return response;
-  //       }
-  //       console.log('event.request', event.request)
-  //       return fetch(event.request);
-  //     })
-  // )
-// })
+self.addEventListener('activate', function(event) {
+  // Lakukan aktifasi
+  });
 
 self.addEventListener('fetch', (event) => {
-  console.log('Fetching:', event.request.url)
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        console.log('isi response ==========', response)
         if(response) {
-          // console.log(`Found ${event.request.url} in cache`)
           return response;
         }
-        // console.log(`Network request for ${event.request.url}`)
-
-        var fetchRequest = event.request.clone();
-        console.log('fetchrequest', fetchRequest)
-        return fetch(fetchRequest).then(
-          function(response) {
-            console.log('response', response)
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
+        return fetch(event.request).then(response => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request.url, response.clone());
             return response;
-          }
-        );
-        // return fetch(event.request).then(response => {
-        //   return caches.open(filesToCache).then(cache => {
-        //     cache.put(event.request.url, response.clone());
-        //     return response;
-        //   });
-        // });
+          });
+        });
       })
       .catch(error => {
         console.log(error)
@@ -87,3 +47,39 @@ self.addEventListener('fetch', (event) => {
   )
 })
 
+// self.addEventListener('fetch', (event) => {
+//   console.log('Fetching:', event.request.url)
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(response => {
+//         console.log('isi response ==========', response)
+//         if(response) {
+//           // console.log(`Found ${event.request.url} in cache`)
+//           return response;
+//         }
+//         // console.log(`Network request for ${event.request.url}`)
+
+//         var fetchRequest = event.request.clone();
+//         console.log('fetchrequest', fetchRequest)
+//         return fetch(fetchRequest).then(
+//           function(response) {
+//             console.log('response', response)
+//             if(!response || response.status !== 200 || response.type !== 'basic') {
+//               return response;
+//             }
+//             var responseToCache = response.clone();
+
+//             caches.open(CACHE_NAME)
+//               .then(function(cache) {
+//                 cache.put(event.request, responseToCache);
+//               });
+
+//             return response;
+//           }
+//         );
+//       })
+//       .catch(error => {
+//         console.log(error)
+//       })
+//   )
+// })
